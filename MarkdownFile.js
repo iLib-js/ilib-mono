05a9cddd726f1ee95e133cbbb877283a676f4649
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2020-06-27 02:09:27 -0700

    Put back missing require()

--- MarkdownFile.js
+++ MarkdownFile.js
@@ -21,6 +21,7 @@ var fs = require("fs");
 var path = require("path");
 var log4js = require("log4js");
 var MessageAccumulator = require("message-accumulator").default;
+var Node = require("ilib-tree-node").default;
 var Locale = require("ilib/lib/Locale.js");
 var isAlnum = require("ilib/lib/isAlnum.js");
 var isIdeo = require("ilib/lib/isIdeo.js");
