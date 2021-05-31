1464ba22b281dc3c39b435c1b7f1924cfebe0a47
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2021-05-30 22:23:41 -0700

    Fix pseudo locale generation

--- ObjectiveCFileType.js
+++ ObjectiveCFileType.js
@@ -43,7 +43,7 @@ var ObjectiveCFileType = function(project) {
     this.pseudos = {};
 
     // generate all the pseudo bundles we'll need
-    project.locales && project.locales.forEach(function(locale) {
+    project.settings && project.settings.locales && project.settings.locales.forEach(function(locale) {
         var pseudo = this.API.getPseudoBundle(locale, this, project);
         if (pseudo) {
             this.pseudos[locale] = pseudo;
