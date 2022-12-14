514dd6ed9aab0f553053f796a510b76a074925fb
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2022-12-14 12:55:58 -0800

    Moved from from i18nlint so the plugins can use them
    - no changes from i18nlint
    - intended to be used both by the program and the plugins

---
+++ src/index.js
@@ -0,0 +1,26 @@
+/*
+ * index.js - export everything from all of the files
+ *
+ * Copyright © 2022 JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Formatter from './Formatter.js';
+import Parser from './Parser.js';
+import Plugin from './Plugin.js';
+import Result from './Result.js';
+import Rule from './Rule.js';
+
+export { Formatter, Parser, Plugin, Result, Rule };
