4da4898c422879c57e6bd11d424bc95910c97de2
Edwin Hoogerbeets <ehoogerbeets@box.com>
2023-09-25 15:50:49 -0700

    Added all the text from the real file
    - next, we whittle it down to just the essense to see what fails
    and what doesn't

--- test/MarkdownFile.test.js
+++ test/MarkdownFile.test.js
@@ -1544,24 +1544,156 @@ code code code
         });
         expect(mf).toBeTruthy();
         mf.parse(`
-## This is a header
+## Write metadata cards to file
 
-Follow these steps:
+To write one or more cards to a file, you can use the [\`POST
+/files/:id/metadata/global/boxSkillsCards\`](e://post_files_id_metadata_global_boxSkillsCards)
+API and pass along a list of Box Skill \`cards\`.
 
-1. First point:
-\`\`\`
-code code code
+<!-- markdownlint-enable line-length -->
+
+<Tabs>
+
+<Tab title='cURL'>
+
+\`\`\`curl
+curl -X POST https://api.box.com/2.0/files/12345/metadata/global/boxSkillsCards \
+     -H 'Authorization: Bearer <ACCESS_TOKEN>'
+     -H 'Content-Type: application/json'
+     -d '{
+       "cards": [{
+         "type": "skill_card",
+         "skill_card_type": "keyword",
+         "skill_card_title": {
+           "code": "license-plates",
+           "message": "Licence Plates"
+         },
+         "skill": {
+           "type": "service"
+           "id": "license-plates-service"
+         },
+         "invocation": {
+           "type": "skill_invocation"
+           "id": "license-plates-service-123"
+         },
+         "entries": {
+           { "text": "DD-26-YT" },
+           { "text": "DN86 BOX" }
+         }
+       }],
+     }'
 \`\`\`
 
-1. Second point:
-\`\`\`
-code code code
+</Tab>
+
+<Tab title='Node'>
+
+\`\`\`js
+const metadata = { 
+  cards: [{
+    "type": "skill_card",
+    "skill_card_type": "keyword",
+    "skill_card_title": {
+      "code": "license-plates",
+      "message": "Licence Plates"
+    },
+    "skill": {
+      "type": "service"
+      "id": "license-plates-service"
+    },
+    "invocation": {
+      "type": "skill_invocation"
+      "id": "license-plates-service-123"
+    },
+    "entries": {
+      { "text": "DD-26-YT" },
+      { "text": "DN86 BOX" }
+    }
+  }] 
+}
+
+client.files.addMetadata('12345', 'global', 'boxSkillsCards', metadata)
+  .then(metadata => { 
+    // ...
+  })
 \`\`\`
 
-1. Third point:
+</Tab>
+
+<Tab title='Python'>
+
+\`\`\`py
+metadata = { 
+  cards: [{
+    "type": "skill_card",
+    "skill_card_type": "keyword",
+    "skill_card_title": {
+      "code": "license-plates",
+      "message": "Licence Plates"
+    },
+    "skill": {
+      "type": "service"
+      "id": "license-plates-service"
+    },
+    "invocation": {
+      "type": "skill_invocation"
+      "id": "license-plates-service-123"
+    },
+    "entries": {
+      { "text": "DD-26-YT" },
+      { "text": "DN86 BOX" }
+    }
+  }] 
+}
+
+client.file(file_id='12345').metadata(scope='global', template='boxSkillsCards').create(metadata)
 \`\`\`
-code code code
+</Tab>
+
+<Tab title='Java'>
+
+\`\`\`java
+BoxFile file = new BoxFile(api, "12345");
+Metadata metadata = new Metadata()
+file.createMetadata("global", "boxSkillsCards", metadata);
 \`\`\`
+
+</Tab>
+
+<Tab title='.NET'>
+
+\`\`\`cs
+var metadataValues = new Dictionary<string, object>()
+{ 
+  cards: [{
+    "type": "skill_card",
+    "skill_card_type": "keyword",
+    "skill_card_title": {
+      "code": "license-plates",
+      "message": "Licence Plates"
+    },
+    "skill": {
+      "type": "service"
+      "id": "license-plates-service"
+    },
+    "invocation": {
+      "type": "skill_invocation"
+      "id": "license-plates-service-123"
+    },
+    "entries": {
+      { "text": "DD-26-YT" },
+      { "text": "DN86 BOX" }
+    }
+  }] 
+};
+Dictionary<string, object> metadata = await client.MetadataManager
+    .CreateFileMetadataAsync(fileId: "12345", metadataValues, "global", "boxSkillsCards");
+\`\`\`
+
+</Tab>
+
+</Tabs>
+
 `
         );
 
