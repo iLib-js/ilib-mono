471503da31e1dc089b4e97b7ebe4133a06eace21
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2023-11-21 22:08:28 -0800

    Add rel notes

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,24 +1,24 @@
-/* 
- * generate.js - tool to generate plurals json fragments from  
- * the CLDR data files 
- *  
+/*
+ * generate.js - tool to generate plurals json fragments from
+ * the CLDR data files
+ *
  * Copyright © 2015-2017, 2020-2023 JEDLSoft
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-/* 
- * This code is intended to be run under node.js  
+/*
+ * This code is intended to be run under node.js
  */
 import fs from 'fs';
 import path from 'path';
