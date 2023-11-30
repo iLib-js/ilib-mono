c02e9c1e04e706c7036cc388915c834b6616092a
Wadim Wawrzenczak <wwawrzenczak@box.com>
2023-11-30 15:01:07 +0100

    Add basic tests for Flow+JSX files

--- test/rules/BanFormattedCompMessage.test.js
+++ test/rules/BanFormattedCompMessage.test.js
@@ -17,7 +17,93 @@
  * limitations under the License.
  */
 
-describe("BanFormattedCompMessage", () => {
-    
+import FlowParser from "../../src/parsers/FlowParser.js";
+import BanFormattedCompMessage from "../../src/rules/BanFormattedCompMessage.js";
+import { trimIndent } from "../utils.js";
 
+/** @typedef {import("i18nlint-common").IntermediateRepresentation} IntermediateRepresentation */
+
+describe("BanFormattedCompMessage", () => {
+    describe("Flow JSX", () => {
+        const getFlowJsxIr = (
+            /** @type {string} */ filePath,
+            /** @type {string} */ content
+        ) => {
+            const parser = new FlowParser();
+            parser.data = trimIndent(content);
+            parser.path = filePath;
+            const [ir] = parser.parse();
+            return ir;
+        };
+
+        test("component used in file", () => {
+            const ir = getFlowJsxIr(
+                "x/y.js",
+                `
+                // @flow
+                import * as React from "react";
+                import { Button, Link, FormattedCompMessage } from "components";
+                
+                export class CustomComponent extends React.Component {
+                    render() {
+                        return (
+                            <>
+                                <FormattedCompMessage id="some.id" description="Some message description">
+                                    Some message with <Link href="example.com">link text</Link> and more text.
+                                </FormattedCompMessage>
+                                <Button className="some-button" type="button">
+                                    Click me
+                                </Button>
+                            </>
+                        );
+                    }
+                }
+                `
+            );
+
+            const rule = new BanFormattedCompMessage();
+
+            const result = rule.match({ ir });
+
+            expect(result.length).toEqual(2);
+        });
+
+        test("component not used in file", () => {
+            const ir = getFlowJsxIr(
+                "x/y.js",
+                `
+                // @flow
+                import * as React from "react";
+                import { Button, Link, FormattedMessage } from "components";
+
+                export class CustomComponent extends React.Component {
+                    render() {
+                        return (
+                            <>
+                                <FormattedMessage
+                                    id="some.id"
+                                    description="Some message description"
+                                    defaultMessage="Some message with <Link>link text</Link> and more text."
+                                    values={{
+                                        Link: (chunks) => <Link href="example.com">chunks</Link>,
+                                    }}
+                                />
+                                <Button className="some-button" type="button">
+                                    Click me
+                                </Button>
+                            </>
+                        );
+                    }
+                }
+
+                `
+            );
+
+            const rule = new BanFormattedCompMessage();
+
+            const result = rule.match({ ir });
+
+            expect(result.length).toEqual(0);
+        });
+    });
 });
