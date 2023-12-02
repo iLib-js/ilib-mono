0cc6885e10e56776866c0a30dc51a70e40351f8e
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2023-12-01 16:09:40 -0800

    clean up

--- karma-setup.js
+++ karma-setup.js
@@ -1,8 +1,3 @@
-// the jest.fn() API
-// import jest from "jest";
-// The matchers API
-// import {expect, jest, test, beforeAll, beforeEach, afterAll, afterEach} from 'jest-without-globals';
-
 // Add missing Jest functions
 window.test = window.it;
 window.test.each = (inputs) => (testName, test) =>
@@ -13,11 +8,3 @@ window.test.todo = function () {
 
 window.expect.assertions = (num) => { return undefined; };
 
-/*
-window.jest = jest;
-window.expect = expect;
-window.beforeAll = beforeAll;
-window.beforeEach = beforeEach;
-window.afterAll = afterAll;
-window.afterEach = afterEach;
-*/
