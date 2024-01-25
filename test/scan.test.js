f0c2f335afc3f01d6b7de52035cca2227504a9e3
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2024-01-25 15:08:07 -0800

    Convert all unit tests from nodeunit to jest

--- test/scan.test.js
+++ test/scan.test.js
@@ -1,7 +1,7 @@
 /*
- * testscan.js - test the assemble utility
+ * scan.test.js - test the assemble utility
  *
- * Copyright © 2022 JEDLSoft
+ * Copyright © 2022, 2024 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,55 +19,47 @@
 
 import scan, { scanString } from '../src/scan.js';
 
-export const testscan = {
-    testScanStringImportStatement: function(test) {
-        test.expect(2);
+describe("testscan", () => {
+    test("ScanStringImportStatement", () => {
+        expect.assertions(2);
         let set = new Set();
         scanString(`import something from 'ilib-something';`, set);
-        test.equal(set.size, 1);
-        test.ok(set.has("ilib-something"));
+        expect(set.size).toBe(1);
+        expect(set.has("ilib-something")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanStringImportCall: function(test) {
-        test.expect(2);
+    test("ScanStringImportCall", () => {
+        expect.assertions(2);
         let set = new Set();
         scanString(`function foo(asdf) {
                 return import("ilib-locale");
             };`, set);
-        test.equal(set.size, 1);
-        test.ok(set.has("ilib-locale"));
+        expect(set.size).toBe(1);
+        expect(set.has("ilib-locale")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanStringStringRequireCall: function(test) {
-        test.expect(2);
+    test("ScanStringStringRequireCall", () => {
+        expect.assertions(2);
         let set = new Set();
         scanString(`function foo(asdf) {
                 return require("ilib-locale");
             };`, set);
-        test.equal(set.size, 1);
-        test.ok(set.has("ilib-locale"));
+        expect(set.size).toBe(1);
+        expect(set.has("ilib-locale")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanStringWhiteSpaceImportStatement: function(test) {
-        test.expect(2);
+    test("ScanStringWhiteSpaceImportStatement", () => {
+        expect.assertions(2);
         let set = new Set();
         scanString(
             `import something      from    'ilib-something'   ;
             `, set);
-        test.equal(set.size, 1);
-        test.ok(set.has("ilib-something"));
+        expect(set.size).toBe(1);
+        expect(set.has("ilib-something")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanStringWhiteSpaceImportCall: function(test) {
-        test.expect(2);
+    test("ScanStringWhiteSpaceImportCall", () => {
+        expect.assertions(2);
         let set = new Set();
         scanString(
             `function foo(asdf) {
@@ -75,51 +67,43 @@ export const testscan = {
                     // do somethign with the module
                 });
             };`, set);
-        test.equal(set.size, 1);
-        test.ok(set.has("ilib-locale"));
+        expect(set.size).toBe(1);
+        expect(set.has("ilib-locale")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanStringWhiteSpaceRequireCall: function(test) {
-        test.expect(2);
+    test("ScanStringWhiteSpaceRequireCall", () => {
+        expect.assertions(2);
         let set = new Set();
         scanString(
             `function req(asdf) {
                 var name = require   (    "ilib-name"    );
                 // do something with name
             };`, set);
-        test.equal(set.size, 1);
-        test.ok(set.has("ilib-name"));
+        expect(set.size).toBe(1);
+        expect(set.has("ilib-name")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanStringFalseImportStatement: function(test) {
-        test.expect(1);
+    test("ScanStringFalseImportStatement", () => {
+        expect.assertions(1);
         let set = new Set();
         scanString(
             `asdfimport something      from    'ilib-something'   ;
             `, set);
-        test.equal(set.size, 0);
+        expect(set.size).toBe(0);
+    });
 
-        test.done();
-    },
-
-    testScanStringComplexImportStatement: function(test) {
-        test.expect(2);
+    test("ScanStringComplexImportStatement", () => {
+        expect.assertions(2);
         let set = new Set();
         scanString(
             `import something, { somethingElse }      from    'ilib-something'   ;
             `, set);
-        test.equal(set.size, 1);
-        test.ok(set.has("ilib-something"));
+        expect(set.size).toBe(1);
+        expect(set.has("ilib-something")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanStringFalseImportCall: function(test) {
-        test.expect(1);
+    test("ScanStringFalseImportCall", () => {
+        expect.assertions(1);
         let set = new Set();
         scanString(
             `function foo(asdf) {
@@ -127,43 +111,35 @@ export const testscan = {
                     // do somethign with the module
                 });
             };`, set);
-        test.equal(set.size, 0);
+        expect(set.size).toBe(0);
+    });
 
-        test.done();
-    },
-
-    testScanStringFalseRequireCall: function(test) {
-        test.expect(1);
+    test("ScanStringFalseRequireCall", () => {
+        expect.assertions(1);
         let set = new Set();
         scanString(
             `function req(asdf) {
                 var name = bigrequire("ilib-name");
                 // do something with name
             };`, set);
-        test.equal(set.size, 0);
+        expect(set.size).toBe(0);
+    });
 
-        test.done();
-    },
-
-    testScanImportStatement: function(test) {
-        test.expect(3);
+    test("ScanImportStatement", () => {
+        expect.assertions(3);
         let set = new Set();
         scan("./src/index.js", set);
-        test.equal(set.size, 2);
-        test.ok(set.has("ilib-locale"));
-        test.ok(set.has("ilib-common"));
+        expect(set.size).toBe(2);
+        expect(set.has("ilib-locale")).toBeTruthy();
+        expect(set.has("ilib-common")).toBeTruthy();
+    });
 
-        test.done();
-    },
-
-    testScanImportStatementsMultiple: function(test) {
-        test.expect(3);
+    test("ScanImportStatementsMultiple", () => {
+        expect.assertions(3);
         let set = new Set();
         scan("./test/testfiles/importtest.js", set);
-        test.equal(set.size, 2);
-        test.ok(set.has("ilib-something"));
-        test.ok(set.has("ilib-locale"));
-
-        test.done();
-    }
-};
+        expect(set.size).toBe(2);
+        expect(set.has("ilib-something")).toBeTruthy();
+        expect(set.has("ilib-locale")).toBeTruthy();
+    });
+});
