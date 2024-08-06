077c8564ac8efdcdfef5102ef9e5392f3237faaa
Goun Lee <goun.lee@lge.com>
2024-08-06 17:27:56 +0900

    more update

--- src/legacyilibassemble.js
+++ src/legacyilibassemble.js
@@ -29,12 +29,25 @@ let locales;
 let jsFileList = [];
 let allJSList = [];
 
-const dependentsPatterns  = /require\(\s*(\"|\').*\/(.*.js)(\"|]')\)\;/g;
-const reDependentPattern = new RegExp(/require\(\s*(\"|\').*\/(.*.js)(\"|]')\)\;$/, "g");
-const reDataPattern = new RegExp(/\/\/\s*\!data\s*(.*)$/, "g");
+const reDependentPattern = new RegExp(/require\(\s*(\"|\')\.*\/(.*\.js)(\"|\')\)\;/, "g");
+const reDataPattern = new RegExp(/\/\/\s*!data\s*(([^\\])+)/, "g");
+
+const localeIndependent = [
+    "astro",
+    "zoneinfo",
+    "localematch",
+    "ctype",
+    "ctype_l",
+    "ctype_m",
+    "ctype_n",
+    "ctype_p",
+    "ctype_z",
+    "scriptToRange",
+    "pseudomap"
+];
 
 function assembleilib(options) {
-    console.log(options);
+    //console.log(options);
     ilibPath = options.opt.ilibPath;
     ilibincPath = options.opt.ilibincPath;
     locales = options.opt.locales;
@@ -44,44 +57,131 @@ function assembleilib(options) {
 }
 
 function readIncFile(incpath) {
-    const info = fs.readFileSync(incpath, 'utf8');
-    const lines = info.split('\n');
+    let info = readFile(incpath, 'utf8');
+    let lines = info.split('\n');
     lines.forEach(line => {
         if (line.indexOf('js') != -1) {
             jsFileList.push(line);
         }
     });
 }
+
+function readFile(filepath) {
+    let readData = "";
+    if (!filepath) return;
+
+    if (fs.existsSync(filepath)) {
+        readData = fs.readFileSync(filepath, "utf-8");
+        return readData;
+    } else {
+        console.log("The file [" + filepath + "] does not exist.");
+    }
+}
+
 //node --inspect-brk src/index.js result --legacyilib --ilibPath ~/Source/develop/ --ilibincPath src/ilib-assemble-inc.js 
-let extractjs = [];
-let extractdata = [];
+
+let dependentJS = [];
+let dependentData = [];
+let assembleJSAll= "";
+
 function readJSFiles() {
     let readData;
-    let matchesJS;
-    let matchesData;
+    let matchedJS;
+    let matchedData;
     jsFileList.forEach(function(file){
-        let jsPath = path.join(ilibPath, 'js/lib', file);
-        if (fs.existsSync(jsPath)){
-            readData = fs.readFileSync(jsPath, 'utf-8');
+        let jsPath = path.join(ilibPath, "js/lib", file);
+        readData = readFile(jsPath);
 
-            matchesJS = [...readData.matchAll(reDependentPattern)];
-            if (matchesJS.length > 0){
-                matchesJS.forEach(function(item){
-                    if (!extractjs.includes(item[2])) {
-                        extractjs.push(item[2]);
-                    }
-                });
-            }
-            matchesData = [...readData.matchAll(reDataPattern)];
-            if (matchesData.length > 0){
-                matchesData.forEach(function(item){
-                    matchesData.log('//');
-                });
-            }
+        matchedJS = [...readData.matchAll(reDependentPattern)];
+        if (matchedJS.length > 0){
+            matchedJS.forEach(function(item){
+                if (!dependentJS.includes(item[2])) {
+                    dependentJS.push(item[2]);
+                }
+            });
+        }
 
+        matchedData = reDataPattern.exec(readData);
+        if (matchedData !== null){
+            let result = matchedData[1].split('\n')[0].split(' ');
+            result.forEach(function(item){
+                if (!dependentData.includes(item)) {
+                    dependentData.push(item);
+                }
+            });
         }
     });
-    console.log(extractjs);
+
+    allJSList = [...new Set([...dependentJS, ...jsFileList])];
+
+    assemblejs();
+    //assemblelocale();
+}
+
+function deletePatterns(data) {
+    const deletePattern1 = /var\s*[^;]*=[^;]require[^;]*;/g;
+    const deletePattern2 = /module\.exports\s*=\s*[^;]*;/g;
+    data = data.replaceAll(deletePattern1, "").replaceAll(deletePattern2, "");
+    return data;
+}
+
+function assembleLocaleIndepententData() {
+    let readData;
+    let allData;
+    dependentData.forEach(function(data){
+        if (localeIndependent.indexOf(data) != -1) {
+            let dataPath = path.join(ilibPath, "js/data/locale", data + ".json" );
+            //ilib.data.plurals_af = {"one":{"eq":["n",1]}};
+            readData = readFile(dataPath);
+            allData += "ilib.data." + data + " = " + readData;
+        }
+    });
+    return allData;
+}
+
+function assembleZoneinfo() {
+    let readData;
+    let zoneinfoPath = path.join(ilibPath, "js/data/locale/zoneinfo");
+    readData = zoneinfoWalk(zoneinfoPath);
+    return readData;
+}
+
+function zoneinfoWalk(zoneinfoPath) {
+    let dirList = fs.readdirSync(zoneinfoPath);
+
+    let allData = "";
+    dirList.forEach(function(dir) {
+        let filePath = path.join(zoneinfoPath, dir);
+        let stat = fs.statSync(filePath);
+        if (stat && stat.isDirectory()) {
+            zoneinfoWalk(filePath);
+        } else {
+            let timezoneID = filePath.replace(path.join(ilibPath, "js/data/locale/zoneinfo/"), "").replace(".json", "");
+            let readData = readFile(filePath);
+            allData += 'ilib.data.zoneinfo["' + timezoneID + '"] = ' + readData + ';\n';
+        }
+    });
+    return allData;
+}
+
+function assemblejs() {
+    allJSList.forEach(function(file){
+        let filePath;
+        let readData;
+        if (file == 'index.js') {
+            filePath = path.join(ilibPath, "js", file);
+        } else {
+            filePath = path.join(ilibPath, "js/lib", file);
+        }
+        readData = readFile(filePath, 'utf-8');
+        assembleJSAll += readData;
+    });
+
+    assembleJSAll = deletePatterns(assembleJSAll);
+    assembleJSAll += assembleLocaleIndepententData();
+    assembleJSAll += assembleZoneinfo();
+
+    fs.writeFileSync('result.js', assembleJSAll, 'utf-8');
 }
 
 export default assembleilib;
