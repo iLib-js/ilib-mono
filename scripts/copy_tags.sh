7484cf9dde68960283a66569dd38cd6c6f8e8b7a
Edwin Hoogerbeets <ehoogerbeets@box.com>
2024-11-07 14:29:06 -0800

    Clean up copy tags script a bit

--- scripts/copy_tags.sh
+++ scripts/copy_tags.sh
@@ -8,18 +8,6 @@ set -euo pipefail
 URL_TEMPLATE='git@github.com:iLib-js/<repo>.git'
 SUBTREE_PREFIX_TEMPLATE='packages/<repo>'
 
-# exit if running on main branch
-if [ "$(git branch --show-current)" == "main" ]; then
-    echo "Do not run this script on the main branch"
-    exit 1
-fi
-
-# exit if there are uncommitted changes
-#if [ -n "$(git status --porcelain)" ]; then
-#    echo "There are uncommitted changes. Commit or stash them before running this script"
-#    exit 1
-#fi
-
 # for each repository name in arguments
 for REPO in "$@"
 do
@@ -36,13 +24,7 @@ do
 
     # discover default branch name
     BRANCH=$(git remote show "$REMOTE" | grep 'HEAD branch' | cut -d' ' -f5)
-    #echo "Discovered default branch $BRANCH"
-    #git fetch "$REMOTE" "$BRANCH"
-
-    # add subtree for the remote
-    #PREFIX="${SUBTREE_PREFIX_TEMPLATE//<repo>/$REPO}"
-    #echo "Adding subtree $PREFIX"
-    #git subtree add --prefix "$PREFIX" "$REMOTE/$BRANCH"
+    echo "Discovered default branch $BRANCH"
 
     # carry over tags to the new repository
     git ls-remote --tags $REMOTE | grep '{}' | sed 's|refs/tags/||' | sed 's|\^{}||' > tags.txt
@@ -51,8 +33,8 @@ do
         echo "Creating tag $REMOTE-$tag" on commit $COMMIT
         # Create a new tag in the monorepo using the original tag's commit
         git tag -a "$REMOTE-$tag" -m "$REMOTE-$tag" "$COMMIT"
-        git push origin "$REMOTE-$tag"
     done
+    git push origin --tags
 
     # cleanup: remove remote
     git remote remove "$REMOTE"
