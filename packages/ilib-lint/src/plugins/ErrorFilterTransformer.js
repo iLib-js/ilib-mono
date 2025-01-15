14448b6a9883fe048361f32df628b31fd293f7b3
Edwin Hoogerbeets <ehoogerbeets@box.com>
2025-01-14 23:10:57 -0800

    Add ErrorFilterTransformer class
    - not used yet

---
+++ packages/ilib-lint/src/plugins/ErrorFilterTransformer.js
@@ -0,0 +1,66 @@
+/*
+ * ErrorFilterTransformer - transform an intermediate representation
+ * by filtering out errors
+ *
+ * Copyright © 2025 JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ResourceXliff } from 'ilib-tools-common';
+import { IntermediateRepresentation, SourceFile, Transformer, Result, FileStats } from 'ilib-lint-common';
+
+/**
+ * Filter out errors from the intermediate representation.
+ */
+class ErrorFilterTransformer extends Transformer {
+    /**
+     * Create a new transformer instance.
+     */
+    constructor(options) {
+        super(options);
+    }
+    
+    /**
+     * Filter out errors from the intermediate representation.
+     * 
+     * @param {IntermediateRepresentation} ir the intermediate representation to filter
+     * @param {Result[]|undefined} results the results of the linting process
+     * @param {FileStats|undefined} fileStats the statistics of the files
+     * @returns {IntermediateRepresentation} the filtered intermediate representation
+     */
+    transform(ir, results, fileStats) {
+        if (ir.getType() !== 'resource') {
+            return ir;
+        }
+        const resources = ir.getRepresentation();
+        const resultIndex = {};
+        results.forEach(result => {
+            resultIndex[result.id] = result;
+        });
+        const filteredResources = resources.filter(resource => {
+            const result = resultIndex[resource.getKey()];
+            if (result && result.type === 'error') {
+                return false;
+            }
+        });
+        return new IntermediateRepresentation({
+            type: ir.getType(),
+            ir: filteredResources,
+            sourceFile: ir.getSourceFile()
+        });
+    }
+};
+
+export default ErrorFilterTransformer;
