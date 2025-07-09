9a35141dcfdff848c8c376f4e10eaf16303a60ed
Wadim Wawrzenczak <wwawrzenczak@box.com>
2025-07-10 00:14:25 +0200

    Drop unused sample

--- packages/ilib-loctool-salesforce-metaxml/test-e2e/samples.e2e.test.js
+++ packages/ilib-loctool-salesforce-metaxml/test-e2e/samples.e2e.test.js
@@ -2,19 +2,6 @@ const path = require("path");
 const { expectFileToMatchSnapshot, LoctoolRunner } = require("@ilib-mono/e2e-test");
 
 describe("samples", () => {
-    describe("arb", () => {
-        const projectPath = path.resolve(__dirname, "..", "samples", "arb");
-
-        beforeAll(async () => {
-            const loctool = new LoctoolRunner(projectPath);
-            await loctool.run("localize");
-        });
-
-        it("should produce an extracted XLIFF file", () => {
-            const xliffPath = path.resolve(projectPath, "arb-extracted.xliff");
-            expectFileToMatchSnapshot(xliffPath);
-        });
-    });
     describe("salesforce", () => {
         const projectPath = path.resolve(__dirname, "..", "samples", "salesforce");
 
