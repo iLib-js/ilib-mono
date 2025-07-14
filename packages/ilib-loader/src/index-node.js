09825c3791eed09e89716087a5198c5eb5707c4c
Edwin Hoogerbeets <ehoogerbeets@box.com>
2025-07-14 00:13:34 -0700

    Added the ability to have browser-only loaders
    - Browsers can specify to use the webpack loader
      automatically by putting
      `"ilib-loader": "ilib-loader/browser"` in the
      webpack.resolve.alias in the karma configuration.
    - This will avoid dragging in the NodeLoader to the
      webpack bundle, and all of its dependencies making
      for a clean webpack build
    - reorganized the tests for Node and Browsers in separate files
      so that they can be excluded while running the tests for the
      other platform

---
+++ packages/ilib-loader/src/index-node.js
@@ -0,0 +1,26 @@
+/*
+ * index-node.js - Node.js specific entry point for ilib-loader
+ *
+ * Copyright © 2022, 2025 JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Loader, registerLoader, LoaderFactory } from './LoaderFactory.js';
+import NodeLoader from './NodeLoader.js';
+
+registerLoader(NodeLoader);
+
+export { Loader, registerLoader };
+export default LoaderFactory; 
