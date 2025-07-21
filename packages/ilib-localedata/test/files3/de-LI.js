c8ec44a212aae2842ab311c9b08c3d9034fa9694
Edwin Hoogerbeets <ehoogerbeets@box.com>
2025-07-21 16:04:46 -0700

    Resolved a problem with asynch data loading
    - If too much data was loaded at once, ensureLocale would sometimes
    have a Promise that would resolve to "true" when the data was not
    yet parsed and cached, leading to a situation where code would
    continue assuming the locale data was there.
    - Had to call ensureLocale with many locales hundreds of times to
    consistently repro the situation and show that it was fixed

---
+++ packages/ilib-localedata/test/files3/de-LI.js
@@ -0,0 +1,23 @@
+export default function getLocaleData() {
+    return {
+    "root": {
+        "address": {
+            "region": "Province",
+            "postalCode": "Postal Code",
+            "country": "Country"
+        },
+        "datefmt": {
+            "short": "MM/dd/yyyy",
+            "medium": "MMM dd, yyyy",
+            "long": "MMMM dd, yyyy"
+        }
+    },
+    "de": {
+        "address": {
+            "region": "Bundesland",
+            "postalCode": "PLZ",
+            "country": "Land"
+        }
+    }
+};
+};
