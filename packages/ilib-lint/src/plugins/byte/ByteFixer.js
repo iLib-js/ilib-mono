49a9e50409f42fccc50fc1127ffc3a86fc17ff33
Wadim Wawrzenczak <wwawrzenczak@box.com>
2025-08-29 13:47:28 +0200

    Fix file headers

--- packages/ilib-lint/src/plugins/byte/ByteFixer.js
+++ packages/ilib-lint/src/plugins/byte/ByteFixer.js
@@ -1,7 +1,7 @@
 /*
  * ByteFixer.js
  *
- * Copyright © 2023-2024 JEDLSoft
+ * Copyright © 2025 Box, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- packages/ilib-lint/test/plugins/positional/PositionalFixCommand.test.js
+++ packages/ilib-lint/test/plugins/positional/PositionalFixCommand.test.js
@@ -1,7 +1,7 @@
 /*
  * PositionalFixCommand.test.js
  *
- * Copyright © 2023, 2025 JEDLSoft
+ * Copyright © 2025 Box, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
