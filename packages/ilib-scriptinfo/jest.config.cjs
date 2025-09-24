73a04efa3b0f8a974fed8876feef25724420a09e
Edwin Hoogerbeets <ehoogerbeets@box.com>
2025-09-24 10:00:26 -0700

    Use the new configs

--- packages/ilib-scriptinfo/jest.config.cjs
+++ packages/ilib-scriptinfo/jest.config.cjs
@@ -1,29 +1,11 @@
-const baseConfig = require('../../jest.config.js');
+const { tsJestConfig } = require("ilib-internal");
 
 const config = {
-    ...baseConfig,
+    ...tsJestConfig,
     displayName: {
         name: "ilib-scriptinfo",
         color: "blackBright",
     },
-    preset: 'ts-jest',
-    testMatch: [
-        "**/test/**/*.test.ts",
-        "**/test/**/*.test.js"
-    ],
-    moduleFileExtensions: ['ts', 'js'],
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
-    transform: {
-        '^.+\\.ts$': ['ts-jest', {
-            tsconfig: {
-                types: ['jest', 'node'],
-                module: 'CommonJS',
-                moduleResolution: 'node'
-            }
-        }]
-    }
 }
 
 module.exports = config;
