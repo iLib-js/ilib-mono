dc764244f1e32f4003f082b6b16cd9d15367911e
Goun Lee <goun.lee@lge.com>
2026-03-18 13:35:55 +0900

    Update Changeset and copyright in file

--- packages/ilib-assemble/src/legacyilibassemble.js
+++ packages/ilib-assemble/src/legacyilibassemble.js
@@ -1,7 +1,7 @@
 /*
  * legacyilibassemble.js - assemble of legacy style of ilib files.
  *
- * Copyright © 2024 JEDLSoft
+ * Copyright © 2024, 2026 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
