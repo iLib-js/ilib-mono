b417cade3dacfc40bc79b644a12dbcd86dcc1a5c
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2026-07-05 23:48:24 -0700

    Updated some comments
    - addresses review comments

--- packages/ilib-loader/src/index.js
+++ packages/ilib-loader/src/index.js
@@ -1,8 +1,7 @@
 /*
- * LoaderFactory.js - create new loader objects or return existing
- * ones
+ * index.js - create new loader objects or return existing ones
  *
- * Copyright © 2022, 2025 JEDLSoft
+ * Copyright © 2022, 2025-2026 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
